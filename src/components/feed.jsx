d1fc8b05520845b794dc843cc2af7d2f07655f68
William Alves <williamcostacardoso94@gmail.com>
2023-08-27 22:50:53 -0300

    feat: barra lateral esquerda finalizada

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
-import { Box } from '@mui/material';
-
+import { Box, Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red } from '@mui/material/colors';
 
 export const Feed = () => {
     return(
         <Box bgcolor="pink" flex={4} p={2}>
-            Feed
+            <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+      
+    </Card>
         </Box>
     )
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import GroupsIcon from '@mui/icons-material/Groups';
+import PersonIcon from '@mui/icons-material/Person';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 export const Sidebar = () => {
     return(
-        <Box 
-            bgcolor="skyblue" 
+        <Box  
             flex={1}
             p={2} 
             sx={{ display: { xs: "none", sm: "block" } }}>
@@ -17,6 +23,63 @@ export const Sidebar = () => {
                             <HomeIcon />                
                         </ListItemIcon>
                     <ListItemText primary="Homepage" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <LibraryBooksIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <GroupsIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <AccountBoxIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Profile" />
+                    </ListItemButton>
+                </ListItem>
+                               
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <PersonIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Friends" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <StorefrontIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Marketplace" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <SettingsIcon />                
+                        </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem> 
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <ModeNightIcon />              
+                        </ListItemIcon>
+                        <Switch />
                     </ListItemButton>
                 </ListItem>
             </List>
