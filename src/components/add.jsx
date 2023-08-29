e4f993f062956402ceb23cb79c7d156d4b061add
William Alves <williamcostacardoso94@gmail.com>
2023-08-29 01:57:27 -0300

    feat: inicio do modal

--- src/components/add.jsx
+++ src/components/add.jsx
@@ -1,21 +1,38 @@
-import React from "react";
-import { Tooltip, Fab } from '@mui/material';
+import React, { useState } from "react";
+import { Tooltip, Fab, Box, Typography, Modal } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
 export const Add = () => {
+    const [open, setOpen] = useState(false)
     return(
         <>
-        <Tooltip 
-        title="Delete" 
-        sx={{
-            position:"fixed",
-            bottom: 20,
-            left:{xs:"calc(50%)", md:30},
-            }} >
-            <Fab color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
-      </Tooltip></>
-    )
-}
+            <Tooltip onClick={ e=>setOpen(true) }
+            title="Delete" 
+            sx={{
+                position:"fixed",
+                bottom: 20,
+                left:{xs:"calc(50%)", md:30},
+                }} >
+                <Fab color="primary" aria-label="add">
+                    <AddIcon />
+                </Fab>
+            </Tooltip>
+            <Modal
+                open={open}
+                onClose={ e=>setOpen(false) }
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box 
+                    width={400} 
+                    height={280} 
+                    bgcolor="white" 
+                    p={3}
+                    borderRadius={3}>
+                    
+                </Box>
+            </Modal>
+        </>
+    );
+};
 
