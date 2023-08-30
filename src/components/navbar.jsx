11618a85a17a10e761133344aa8c2c7fbfe0fde2
William Alves <williamcostacardoso94@gmail.com>
2023-08-30 19:57:16 -0300

    feat: arrumando menu do appbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, styled, Box, InputBase, Badge, Avatar, MenuItem, Menu } from '@mui/material';
+import { AppBar, Toolbar, Typography, styled, Box, InputBase, Badge, Avatar, MenuItem, Menu, Button } from '@mui/material';
 import { Mail, Notifications } from '@mui/icons-material'
 import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
 
 const StyledToolbar = styled(Toolbar)({
     display: 'flex',
@@ -34,45 +35,82 @@ const UserBox = styled(Box)(({ theme }) => ({
 }));
 
 export const Navbar = () => {
+
     const [open, setOpen ] = useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const openSide = Boolean(anchorEl);
+    
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return(
         <AppBar position="sticky">
-           <StyledToolbar>
-            <Typography variant='h6' sx={{ display: { xs:"none", sm: "block" }}}>Development</Typography>
-            <MenuIcon sx={{ display: { xs:"block", sm:"none" }}}/>
-            <Search><InputBase placeholder="search..." /></Search>
-            <Icons>
-                <Badge badgeContent={4} color="error">
-                    <Mail />
-                </Badge>
-                <Badge badgeContent={4} color="error">
-                    <Notifications />
-                </Badge>
-                <Avatar onClick={ e => setOpen(true) } sx={{width: 30, height: 30}}  src=""/>
-            </Icons>
-            <UserBox onClick={ e => setOpen(true) }>
-                <Avatar sx={{width: 30, height: 30}}  src=""/>
-                <Typography variant="span" >John</Typography>
-            </UserBox>
-        </StyledToolbar>
-           <Menu
-            id="demo-positioned-menu"
-            aria-labelledby="demo-positioned-button"
-            open={open}
-            onClose={ e => setOpen(false) }            
-            anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <MenuItem>Profile</MenuItem>
-        <MenuItem>My account</MenuItem>
-        <MenuItem>Logout</MenuItem>
-      </Menu>
+            <StyledToolbar>
+                <Typography variant='h6' sx={{ display: { xs:"none", sm: "block" }}}>Development</Typography>
+                            
+                    <MenuIcon
+                        sx={{ display: {  sm: "block" }}}
+                        id="basic-button"
+                        aria-controls={openSide ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={openSide ? 'true' : undefined}
+                        onClick={handleClick}/>
+                    <Menu
+                        id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={openSide}
+                            onClose={handleClose}
+                            MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                            }} >
+
+                            <MenuItem onClick={handleClose}>Homepage</MenuItem>
+                            <MenuItem onClick={handleClose}>Pages</MenuItem>
+                            <MenuItem onClick={handleClose}>Groups</MenuItem>
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>Friends</MenuItem>
+                            <MenuItem onClick={handleClose}>Marketplace</MenuItem>
+                            <MenuItem onClick={handleClose}>Settings</MenuItem>
+                    </Menu>
+                    
+                <Search><InputBase placeholder="search..." /></Search>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={4} color="error">
+                        <Notifications />
+                    </Badge>
+                    <Avatar onClick={ e => setOpen(true) } sx={{width: 30, height: 30}}  src=""/>
+                </Icons>
+                <UserBox onClick={ e => setOpen(true) }>
+                    <Avatar sx={{width: 30, height: 30}}  src=""/>
+                    <Typography variant="span" >John</Typography>
+                </UserBox>
+            </StyledToolbar>
+            <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    open={open}
+                    onClose={ e => setOpen(false) }            
+                    anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+                }}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
         </AppBar>
     )
 }
---
+++ src/components/sidebar-menu.jsx
@@ -0,0 +1,7 @@
+
+
+export const SidebarMenu = ({ active }) => {
+    return (
+       <div>teste</div> 
+    )
+}
